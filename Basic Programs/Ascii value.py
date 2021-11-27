7f1ee1f004550f2e3ba193b659ceeba2827fc0e0
amohata007 <amohata.007@gmail.com>
2021-11-27 16:42:27 +0530

    Basic Programs

---
+++ Basic Programs/Ascii value.py
@@ -0,0 +1,7 @@
+# n = input("Enter any character:")
+# print(ord(n))
+
+
+#ascii value to character
+n = int(input("Enter any number:"))
+print(chr(n))
---
+++ Basic Programs/sum of squares.py
@@ -0,0 +1,13 @@
+# n = int(input("Enter any number:"))
+# s = 0
+# for i in range(1,n+1):
+#     s += i*i
+# print(s)
+
+#O(1) complexity
+# n = int(input("Enter any number:"))
+# print((n*(n+1)*(2*n+1))//6)
+
+#formula of sum of cubes o(1) complexity
+n = int(input("Enter any number:"))
+print((n*n)*((n+1)*(n+1))//4)
