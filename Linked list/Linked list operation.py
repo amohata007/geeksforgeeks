c3865a245dec5d01ad163480bf8fa581638c2ab6
amohata007 <amohata.007@gmail.com>
2021-12-19 22:01:13 +0530

    Linked list

--- Linked list/Linked list operation.py
+++ Linked list/Linked list operation.py
@@ -1,7 +1,8 @@
 #Like arrays, Linked List is a linear data structure. Unlike arrays,
 # linked list elements are not stored at a contiguous location; the elements are linked using pointers.
+
 class Node:
-    def __init__(self,data,next):
+    def __init__(self,data=None,next=None):
         self.data = data
         self.next = next
 
@@ -14,21 +15,22 @@ class LinkedList:
         self.head = node
 
     def print_LL(self):
+        temp = self.head
+        st = ''
         if self.head is None:
-            print("Linked List is empty")
+            print("Linked List is empty.")
         else:
-            temp = self.head
-            st = ''
             while temp:
-                st += str(temp.data) + '-->'
+                pattern = ''
+                if temp.next:
+                    pattern = '-->'
+                st += str(temp.data) + pattern
                 temp = temp.next
             print(st)
-
-
-
 
 var = LinkedList()
 var.insert_at_beginning(10)
 var.insert_at_beginning(20)
 var.insert_at_beginning(30)
+var.insert_at_beginning(40)
 var.print_LL()
